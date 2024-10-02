f8e692044fc760319765bdff602d3e448b2cdfb3
Luke Vasilion <lukevasilion@gmail.com>
2024-10-01 22:17:36 -0400

    base template setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Home', url: 'replace', icon: 'search' },
+    { title: 'AMA Search', url: 'replace', icon: 'search' },
+    { title: 'Track Finder', url: 'replace', icon: 'locate' },
+    { title: 'Favorite Tracks', url: 'replace', icon: 'star' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
