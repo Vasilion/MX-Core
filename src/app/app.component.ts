119ac2087739e4b4da75c7ab310cf25726bafd7c
Luke Vasilion <lukevasilion@gmail.com>
2024-10-02 10:40:21 -0400

    more templating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: 'replace', icon: 'search' },
+    { title: 'Home', url: 'home', icon: 'search' },
     { title: 'AMA Search', url: 'replace', icon: 'search' },
     { title: 'Track Finder', url: 'replace', icon: 'locate' },
     { title: 'Favorite Tracks', url: 'replace', icon: 'star' },
